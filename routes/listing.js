c483a8848622d446828f648ffd65ff3687549c6c
devaaaryan <aaaryandev@gmail.com>
2025-10-11 18:13:14 +0530

    updated code

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { listingSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing");
+const passport = require("passport");
+const { isLoggedIn } = require("../middlewares.js");
 
 
 const validateListing = (req, res, next) => {
@@ -24,7 +26,7 @@ router.get("/", validateListing, wrapAsync(async (req, res) => {
 
 
 //New Route
-router.get("/new", wrapAsync(async (req, res) => {
+router.get("/new", isLoggedIn, wrapAsync(async (req, res) => {
     res.render("./listings/new.ejs");
 })
 );
@@ -34,7 +36,7 @@ router.get("/new", wrapAsync(async (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
-    if(!listing){
+    if (!listing) {
         req.flash("error", "Listing does not exist");
         return res.redirect("/listings");
     }
@@ -44,7 +46,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 );
 
 //Create Route
-router.post("/", wrapAsync(async (req, res, next) => {
+router.post("/", isLoggedIn, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     req.flash("success", "New Listing Created!");
@@ -54,10 +56,10 @@ router.post("/", wrapAsync(async (req, res, next) => {
 );
 
 //edit route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-     if(!listing){
+    if (!listing) {
         req.flash("error", "Listing does not exist");
         return res.redirect("/listings");
     }
@@ -66,10 +68,10 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 );
 
 //Update Route
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-        req.flash("success", "New Listing Updated!");
+    req.flash("success", "New Listing Updated!");
 
     res.redirect(`/listings/${id}`);
 })
@@ -77,10 +79,10 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 
 
 //DELETE Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
-        req.flash("success", "Listing Deleted!");
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
 })
 );
--- routes/user.js
+++ routes/user.js
@@ -32,6 +32,16 @@ router.post("/login", passport.authenticate("local", { failureRedirect: '/login'
     res.redirect("/listings");
 })
 
+router.get("/logout", (req,res)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        res.redirect("/listings");
+    })
+})
+
 
 
 
