d6880d14feeb90a1e6681c598d16eec83ebd15c5
devaaaryan <aaaryandev@gmail.com>
2025-10-11 16:46:36 +0530

    updated code

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -13,17 +14,25 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registerUser = await User.register(newUser, password);
-        console.log(registerUser);
         req.flash("success", "Welcome to Havenly!");
         res.redirect("/listings");
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
     }
-    
+
 }));
 
-router.get("/login", (req,res)=>{
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 })
+
+router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async (req, res) => {
+    req.flash("success", "Welcome Back To Havenly!");
+    res.redirect("/listings");
+})
+
+
+
+
 module.exports = router;
